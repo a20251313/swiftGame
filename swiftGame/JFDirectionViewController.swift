6fd542eeb179c7398543a1993a1343571f8d9256
jingfuran <jingfuran@jingfurandeMacBook-Pro.local>
2014-10-14 09:35:18 +0800

    upload on 20141014

--- swiftGame/JFDirectionViewController.swift
+++ swiftGame/JFDirectionViewController.swift
@@ -9,9 +9,9 @@
 import Foundation
 import UIKit
 
-enum birdDirection
+enum birdDirection:Int
 {
-    case left,right,up,down;
+    case left = 1,right,up,down;
     func desprition()->String
     {
         switch self
@@ -34,23 +34,27 @@ class JFDirectionViewController: UIViewController
     var fromPoint:CGPoint = CGPointZero;
     var endPoint:CGPoint = CGPointZero;
     var birdDire:birdDirection = birdDirection.left;
-    
+    var bgBirdView:UIView?
+    var labelScore:UILabel?
+    var scroe:Int = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: true);
         UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: UIStatusBarAnimation.Fade);
-        
-        var label:UILabel = UILabel(frame: CGRectMake(0, 100, 100, 35))
-        label.backgroundColor = UIColor.lightGrayColor();
-        label.textColor = UIColor.blackColor();
-        label.text = "Test Swift";
-        self.view.addSubview(label)
-        
         self.view.backgroundColor = UIColor.lightGrayColor();
-        
-        
-        var tapGest:UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: "swipeFinger:")
+        let tapGest:UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: "swipeFinger:")
         self.view.addGestureRecognizer(tapGest)
+        
+        [self .setCenterBireDir(birdDirection.up)];
+        
+        labelScore = UILabel(frame: CGRectMake(0, 80, self.view.frame.size.width-20, 30));
+        labelScore?.backgroundColor = UIColor .clearColor();
+        labelScore?.text = "0";
+        self.view.addSubview(labelScore!)
+        labelScore?.textAlignment = NSTextAlignment.Right;
+        labelScore?.textColor = UIColor.blackColor();
+        
+        
     }
     override func loadView() {
      super.loadView()
@@ -58,6 +62,10 @@ class JFDirectionViewController: UIViewController
     
     
 
+    func testLocal(extername localname:String)
+    {
+        println(localname);
+    }
     
     func swipeFinger(gest:UIPanGestureRecognizer?)
     {
@@ -79,8 +87,6 @@ class JFDirectionViewController: UIViewController
         case UIGestureRecognizerState.Failed:
                 endPoint = infoPoint;
             
-            
-  
         }
         println(gest);
     }
@@ -111,6 +117,91 @@ class JFDirectionViewController: UIViewController
         }
         
         println("current direction:\(birdDire.desprition())")
+        var randseed:UInt32 = (UInt32)(NSDate().timeIntervalSince1970);
+        srand(randseed);
+        var randDir:Int = random()%4+1;
+     
+        var dir:birdDirection = birdDirection.fromRaw(randDir)!;
+           println("+++++++++++++++++++dir:\(dir.desprition())+++++++++++++\n\n\n");
+        [self .setCenterBireDir(dir)];
+        
+        
+    }
+    
+    deinit
+    {
+        println("JFDirectionViewController deinit");
+    }
+    
+    func setCenterBireDir(dir:birdDirection)
+    {
+        var sizeWithPng:CGSize = UIImage(named: "lostInMigration_birdDown.png").size;
+       // sizeWithPng = CGSizeMake(sizeWithPng.width/2, sizeWithPng.height/2);
+        var fsep:CGFloat = 10;
+        var fwidth:CGFloat = sizeWithPng.width*3+fsep*2;
+        if bgBirdView == nil
+        {
+            bgBirdView = UIView(frame: CGRectMake(0, 0, fwidth, fwidth));
+            bgBirdView?.backgroundColor = UIColor.clearColor()
+            bgBirdView?.center = self.view.center;
+            self.view.addSubview(bgBirdView!);
+        }
+        
+        var imageForCenter:UIImage? = UIImage(named: "lostInMigration_birdDown.png");
+        switch dir
+        {
+        case .left:
+            imageForCenter = UIImage(named: "lostInMigration_birdLeft.png");
+        case .right:
+            imageForCenter = UIImage(named: "lostInMigration_birdRight.png");
+        case .up:
+            imageForCenter = UIImage(named: "lostInMigration_birdUp.png");
+        case .down:
+            imageForCenter = UIImage(named: "lostInMigration_birdDown.png");
+        }
+        for  item in bgBirdView!.subviews
+        {
+            if(item as? UIView == bgBirdView)
+            {
+                continue;
+            }
+            
+            item.removeFromSuperview();
+            
+        }
+        
+        for i in 0..<5
+        {
+            var fxpoint:CGFloat = 0;
+            var fypoint:CGFloat = 0;
+            var birdView:UIImageView?
+            switch i
+            {
+            case 0:
+                fxpoint = (bgBirdView!.frame.size.width-sizeWithPng.width)/2;
+                fypoint = 0;
+            case 1:
+                fxpoint = 0;
+                fypoint = sizeWithPng.height+fsep;
+            case 2:
+                fxpoint = sizeWithPng.width+fsep;
+                fypoint = sizeWithPng.height+fsep;
+            case 3:
+                fxpoint = sizeWithPng.width*2+fsep*2;
+                fypoint = sizeWithPng.height+fsep;
+            case 4:
+                fxpoint = (bgBirdView!.frame.size.width-sizeWithPng.width)/2;
+                fypoint = sizeWithPng.height*2+fsep*2;
+            default:
+                break;
+            }
+            
+            birdView = UIImageView(frame: CGRectMake(fxpoint, fypoint, sizeWithPng.width, sizeWithPng.height));
+            bgBirdView?.addSubview(birdView!);
+            birdView?.image = imageForCenter;
+            
+        }
+ 
         
     }
 }
